508682df580d5b1555d63651bd04dbc5f61962fa
shahan210 <mohammedshahan210@gmail.com>
2024-04-05 13:02:29 +0530

    chore : database configured

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,24 @@
 import express from "express";
 import cors from "cors";
 const app = express();
+import db from "./db.js";
 
 // middleWare
-
 app.use(cors());
 
 app.use(express.json());
+
+//get all
+
+//get specific
+
+//create
+
+//update
+
+//delete
+
+//delete all
 
 app.listen(3000, () => {
   console.log("Port is running in 3000");
